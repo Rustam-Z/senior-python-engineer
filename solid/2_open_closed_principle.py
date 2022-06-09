279c183dd374551c615a5b1ff9b972e952f573ef
Rustam Zokirov <101385996+RustamZokirovTR@users.noreply.github.com>
2022-06-09 17:35:16 +0500

    Open Close Principle

---
+++ solid/2_open_closed_principle.py
@@ -0,0 +1,59 @@
+"""
+Open Closed Principle
+- Software entities (classes, modules, functions) should be open for extension, but closed for modification.
+
+https://www.pythontutorial.net/python-oop/python-open-closed-principle/
+https://github.com/heykarimoff/solid.python/blob/master/2.ocp.py
+
+If you want to add new features in the future, you should implement the abstract class.
+In our example, Animal class is abstract, it is open for extensions such as Snake, Lion and Mice.
+"""
+
+from abc import ABC, abstractmethod
+
+
+class Animal(ABC):
+    def __init__(self, name: str):
+        self.name = name
+
+    def get_name(self) -> str:
+        ...
+
+    @abstractmethod
+    def make_sound(self) -> str:
+        pass
+
+
+class Lion(Animal):
+    def make_sound(self) -> str:
+        return 'roar'
+
+
+class Mouse(Animal):
+    def make_sound(self) -> str:
+        return 'squeak'
+
+
+class Snake(Animal):
+    def make_sound(self) -> str:
+        return 'hiss'
+
+
+def get_animal(name: str) -> Animal:
+    """The factory method"""
+    animals = dict(dog=Lion("Asad"), cat=Mouse("Nizo"), snake=Snake("Rust"))
+    return animals[name]
+
+
+def animal_sound(animal: Animal):
+    return animal.make_sound()
+
+
+def main():
+    animal = get_animal("snake")
+    print(animal_sound(animal))
+
+
+if __name__ == "__main__":
+    main()
+
