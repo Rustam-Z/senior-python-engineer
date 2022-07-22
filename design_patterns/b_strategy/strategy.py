80099d39bbdbdd30a3b7acd2ac14ae763dde073a
Rustam-Z <zokirovrustam202@gmail.com>
2022-07-22 18:43:22 +0500

    Strategy

--- design_patterns/b_strategy/strategy.py
+++ design_patterns/b_strategy/strategy.py
@@ -10,7 +10,6 @@ import types
 
 class Strategy:
     """The Strategy Pattern class"""
-
     def __init__(self, function=None):
         self.name = "Default Strategy"
 
@@ -23,29 +22,28 @@ class Strategy:
         print("{} is used!".format(self.name))
 
 
-# Replacement method 1
 def strategy_one(self):
     print("{} is used to execute method 1".format(self.name))
 
 
-# Replacement method 2
 def strategy_two(self):
     print("{} is used to execute method 2".format(self.name))
 
 
-# Let's create our default strategy
-s0 = Strategy()
-# Let's execute our default strategy
-s0.execute()
+def main():
+    # Let's create our default strategy
+    s0 = Strategy()
+    s0.execute()
 
-# Let's create the first variation of our default strategy by providing a new behavior
-s1 = Strategy(strategy_one)
-# Let's set its name
-s1.name = "Strategy One"
-# Let's execute the strategy
-s1.execute()
+    s1 = Strategy(strategy_one)
+    s1.name = "Strategy One"
+    s1.execute()
 
-s2 = Strategy(strategy_two)
-s2.name = "Strategy Two"
-s2.execute()
+    s2 = Strategy(strategy_two)
+    s2.name = "Strategy Two"
+    s2.execute()
+
+
+if __name__ == "__main__":
+    main()
 
